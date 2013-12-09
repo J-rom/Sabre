bc900e97bc17aed0df5431eb91bc144cc23e16e9
= <jairamjpatel@gmail.com>
2013-12-08 22:40:28 -0500

    merging stuff

---
+++ saberMotion/src/com/example/lightsaber/HelpActivity.java
@@ -0,0 +1,18 @@
+package com.example.sabre;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help_layout);
+	}	
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+		overridePendingTransition(R.anim.slide_out_left, R.anim.slide_in_left);
+	}	
+}
